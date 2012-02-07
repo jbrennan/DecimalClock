25e0f3ea02ab8415b1a59d9fe2def010494614b2
Jason Brennan <i.jasonbrennan@gmail.com>
2012-02-07 14:02:29 -0500

    Initial Commit

---
+++ DecimalClock/JBAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  JBAppDelegate.m
+//  DecimalClock
+//
+//  Created by Jason Brennan on 12-02-07.
+//  Copyright (c) 2012 Jason Brennan. All rights reserved.
+//
+
+#import "JBAppDelegate.h"
+
+@implementation JBAppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
